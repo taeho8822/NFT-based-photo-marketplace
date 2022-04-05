cad596ac82a00b9cd179dc326a93c79ba714faef
KIM TAEHO <zahir3@nate.com>
2022-04-05 13:58:14 +0900

    ipfs provider 수정

--- client/src/components/ipfs/ipfsApi.js
+++ client/src/components/ipfs/ipfsApi.js
@@ -1,6 +1,6 @@
 //using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server. See IPFS.io docs
 const IPFS = require('ipfs-api');
-const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+const ipfs = new IPFS({ host: 'ec2-3-38-194-111.ap-northeast-2.compute.amazonaws.com', port: 5001, protocol: 'http' });
 
 //run with local daemon
 // const ipfsApi = require('ipfs-api');
